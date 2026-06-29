971e84ddc164782657681995c6614d8a6156c5ea
Merrylax <merrylleboss@gmail.com>
2026-06-29 13:46:58 +0200

    Implement DeathNoteListener class with dependencies

--- src/main/resources/src/main/java/fr/merrylax/deathhealnotes/src/main/java/fr/merrylax/deathhealnotes/items/src/main/java/fr/merrylax/deathhealnotes/src/main/java/fr/merrylax/deathhealnotes/recipes/src/main/java/fr/merrylax/deathhealnotes/models/src/main/java/fr/merrylax/deathhealnotes/listeners/DeathNoteListener.java
+++ src/main/resources/src/main/java/fr/merrylax/deathhealnotes/src/main/java/fr/merrylax/deathhealnotes/items/src/main/java/fr/merrylax/deathhealnotes/src/main/java/fr/merrylax/deathhealnotes/recipes/src/main/java/fr/merrylax/deathhealnotes/models/src/main/java/fr/merrylax/deathhealnotes/listeners/DeathNoteListener.java
@@ -1,1 +1,28 @@
+package fr.merrylax.deathhealnotes.listeners;
 
+import fr.merrylax.deathhealnotes.DeathHealNotes;
+import fr.merrylax.deathhealnotes.items.ItemManager;
+import fr.merrylax.deathhealnotes.managers.DeathManager;
+import org.bukkit.event.Listener;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class DeathNoteListener implements Listener {
+
+    private final DeathHealNotes plugin;
+    private final ItemManager itemManager;
+    private final DeathManager deathManager;
+
+    private final Set<UUID> editingDeathNotes = new HashSet<>();
+
+    public DeathNoteListener(DeathHealNotes plugin) {
+
+        this.plugin = plugin;
+        this.itemManager = plugin.getItemManager();
+        this.deathManager = plugin.getDeathManager();
+
+    }
+
+}
